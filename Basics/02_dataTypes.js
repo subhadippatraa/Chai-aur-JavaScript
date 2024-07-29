42e97221c8160c9198e400e0a423f9c3c77ad9f5
Subhadip Patra <84632003+subhadippatraa@users.noreply.github.com>
2024-07-29 11:35:01 +0000

    DataTypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // js code will be treated as newer version
+
+// alert("HII") //will not be exectuted as we are using node.js not in the browser
+
+
+let name="Subha" //Stirng 
+let age=22 //int
+
+
+
+
+//Types of Data
+
+// ->Primitives Datatypes
+
+// Numbers   to a certain range like 2^53 
+// bigint    to store a larger Number
+// String
+//boolean. true/false
+//null   stand alone value  (This is an object)
+//undefined  the variable value is not assigned yet(but the type will be undefined)
+//symbol  unique
