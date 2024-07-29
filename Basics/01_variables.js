573cb75429af04d6318df9a7f1959167520fb905
Subhadip Patra <84632003+subhadippatraa@users.noreply.github.com>
2024-07-29 11:11:58 +0000

    lec 4

--- Basics/01_variables.js
+++ Basics/01_variables.js
@@ -8,6 +8,10 @@ accountEmail ="subhadippatra@gmail.com"
 accountName="Subhadip Patra"
 accountCtiy="C.K Road"
 
+// as we should'nt use var 
+let accountState; // Gives output as undefined 
+
+
 //Don't use var as this creates some issue with scope and function block
 
-console.table([accountEmail,accountId,accountName,accountCtiy])
+console.table([accountEmail,accountId,accountName,accountCtiy,accountState])
