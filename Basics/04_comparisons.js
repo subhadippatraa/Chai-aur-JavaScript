56d445d433d48119824f59ddf9fbdf6fcfad74a0
Subhadip Patra <84632003+subhadippatraa@users.noreply.github.com>
2024-07-29 14:24:52 +0000

    No

---
+++ Basics/04_comparisons.js
@@ -0,0 +1,11 @@
+// console.log(2>1) // Gives boolean Result
+
+//Comparison between two different dataTypes
+
+// console.log("2"==2);   //also gives true
+
+// console.log("2"===2)//check strictly and gives the expected result as false 
+
+
+console.log(null>=0) //True 
+console.log(null==0) //false 
