af5b6dac6d497b94e9fe8c2c1ffaa057a29eb3b8
Subhadip Patra <84632003+subhadippatraa@users.noreply.github.com>
2024-07-29 12:03:06 +0000

    Conver

---
+++ Basics/03_conOpera.js
@@ -0,0 +1,24 @@
+let rcb=49 //KKR fanboy hahaha
+
+
+// console.log(typeof(rcb));
+
+let rcbStr="49allOUT"
+// console.log(typeof rcbStr);
+
+let x1=null; //it will give 0 as output
+let x2=undefined; //Not a Number
+let x=true //gives 1 and for false gives 0 as output
+
+let pureNum=Number(x); //Now we make sure that the variable pureNum will surely contain a number basically type casing;
+
+console.log( pureNum);
+
+
+// in case of boolean
+    // 1=>true;0=>false;
+//in case of string 
+
+    // " "=>false;
+    // "s"=>true //Not-empty
+
