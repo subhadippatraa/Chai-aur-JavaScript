de9fcacd699cfd23c9ae704efacfe62b146a9b24
Subhadip Patra <84632003+subhadippatraa@users.noreply.github.com>
2024-07-29 11:09:11 +0000

    Lec4

---
+++ Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId= 123455
+let accountName="Subhadip"
+var accountEmail="subhadip@gmail.com"
+accountCtiy="Kharagapur"
+
+// accountId=23456 //Const can't be changed
+accountEmail ="subhadippatra@gmail.com"
+accountName="Subhadip Patra"
+accountCtiy="C.K Road"
+
+//Don't use var as this creates some issue with scope and function block
+
+console.table([accountEmail,accountId,accountName,accountCtiy])
