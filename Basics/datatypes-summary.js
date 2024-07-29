c25a41979163315ab74d5e467d850975800d17cb
Subhadip Patra <84632003+subhadippatraa@users.noreply.github.com>
2024-07-29 14:24:27 +0000

    DataTypes Summary

---
+++ Basics/datatypes-summary.js
@@ -0,0 +1,42 @@
+// Summary of Datatypes
+
+//Primitives Data-Types(Call-by Value)
+
+// They are basically 7 Types
+
+/*
+1.Number
+2.String
+3.Boolean
+4.null
+5.Undefined
+6.Symbol
+7.BigInt
+*/
+
+
+//Non-Primitive Data-Type(Call-by-Reference)
+
+// 1.array
+// 2.object
+// 3.Functions
+
+const id=Symbol('1233')
+const id1=Symbol('1233')
+
+// console.log(id===id1)
+
+//Array Object and functions
+
+const myArray=['subha','subhaas','subham'];
+
+let myObj={
+    Name: Subhadip,
+    Age: 23,
+    Email: "subhadippatra@gmail.com"
+}
+
+const myFun=function(){
+    console.log("Hello World!");
+}
+
