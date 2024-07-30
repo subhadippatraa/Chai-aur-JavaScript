638a97d94a6a6eb61321052b50466dadf73be475
Subhadip Patra <84632003+subhadippatraa@users.noreply.github.com>
2024-07-30 09:49:26 +0000

    Maths

---
+++ Basics/05_string.js
@@ -0,0 +1,18 @@
+let name="Subhadip"
+let age=23
+
+// console.log(`Hello my name is ${name} and my age is ${age}`)  //Have better Redability
+
+const newString= new String("Subhadip")
+console.log(newString.length)
+console.log(newString.toLocaleUpperCase())      //different methods Uppercase,Lowercase and others
+
+
+//There are some method like 
+
+//Trim- To cut down all the spaces
+//Slice-
+//Substring-
+//Replace
+//Split
+
---
+++ Basics/06_maths.js
@@ -0,0 +1,40 @@
+let num=100; 
+let num1=new Number(200) 
+
+// console.log(num.toFixed(2)) //200.00
+// console.log(num.toString().length)
+
+let hundreds=10000000
+// console.log(hundreds.toLocaleString('en-IN'))
+
+//Explore some Methods
+
+/******************** Do Some MATHS ************************** */
+
+let number=-345
+// console.log(Math.abs(number))
+// console.log(number)
+//Explore some methods
+
+// console.log(Math.round(4.85555))  //if its >.5 take take floor otherwise ceil
+
+// console.log(Math.floor(4.55555)) //take flore value 4
+
+// console.log(Math.ceil(4.55555)) //take ceil value 5
+
+
+
+// console.log(Math.random()) //Will generate Random numbers between 0 to 1 both inclusive
+
+let mini=1
+let maxi=6
+
+// let i=0
+// while (i<10) {
+//     console.log(Math.floor(Math.random()*(maxi-mini+1)+mini))
+//     i++;
+    
+// }
+
+
+
